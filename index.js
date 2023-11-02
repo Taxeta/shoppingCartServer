9db42cc191c20213067c78dea77754d8f21105cf
Jose Manuel Vera <josemanuelvera1991@gmail.com>
2023-11-02 13:20:06 +0100

    Modify rewrite petitions

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-const cors = require("cors");
-const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router("phones.json");
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 4002;
-
 const options = {
   writable: true, //esto permite que se sobreescriban los datos
 };
 
-server.use(cors());
+const cors = require("cors");
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("phones.json", options);
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 4002;
+
 server.use(middlewares);
-server.use(router, options);
+server.use(cors());
+server.use(router);
+server.use(jsonServer.router("phones.json", options));
 server.listen(port, () => {
   console.log(`https://localhost:${port}`);
 });
