c25c744b3fc42fe73da775952717d6b0eab18775
Jose Manuel Vera <josemanuelvera1991@gmail.com>
2023-11-02 13:06:45 +0100

    Make the phones.json writable to patch-delete petitions

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const cors = require("cors");
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("phones.json");
+const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 4002;
 
+const options = {
+  writable: true, //esto permite que se sobreescriban los datos
+};
+
 server.use(cors());
-server.use(router);
+server.use(middlewares);
+server.use(router, options);
 server.listen(port, () => {
-  console.log(`${port}`);
+  console.log(`https://localhost:${port}`);
 });
